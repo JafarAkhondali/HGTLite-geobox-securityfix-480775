9b458713f3d7d3d3629e5bedae39707e6786570d
uptonking <jinyaoo86@gmail.com>
2017-03-19 23:01:19 +0800

    [add] dropbox input display

--- box/assets/src/component/DragList.js
+++ box/assets/src/component/DragList.js
@@ -1,26 +1,39 @@
-import React  from 'react';
+import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '../style/styles.scss'
 
-class DragList extends React.Component{
+class DragList extends React.Component {
 
-constructor(){
-    super()
-}
+        constructor() {
+            super()
+        }
 
-render(){
-    return (
-        <ul className="list-group">
-        {
-        React.Children.map(this.props.children,function(child){
-            return <li className="list-group-item no-border">{child}</li>
-        })
-    }
-</ul>
-)
-}
+        generateChosenFiles(files) {
+                let data = [];
+                for (let i = 0, len = files.length; i < len; i++) {
+                    data.push( < li className = "list-group-item no-border"  key={i}> {
+                            files[i].name
+                        } < /li>)
+                    }
+                    if (!data.length) {data.push( < div > 可以把文件拖到这里 < /div>);}
 
-}
+                        return data;
 
-export default DragList
+                    }
+
+                    render() {
+                        let {
+                            inputFiles
+                        } = this.props;
+                        console.log('传过来的文件对象', inputFiles.length,inputFiles);
+
+                        return ( < ul className = "list-group" > {
+
+                                this.generateChosenFiles(inputFiles)
+                            } < /ul>)
+                        }
+
+                    }
+
+                    export default DragList
--- box/assets/src/component/FileTopIndicator.js
+++ box/assets/src/component/FileTopIndicator.js
@@ -1,9 +1,18 @@
-import React  from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom'
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {
+    connect
+}
+from 'react-redux';
+import {
+    bindActionCreators
+}
+from 'redux';
 
-import {Link} from 'react-router';
+import {
+    Link
+}
+from 'react-router';
 import {
     Grid,
     Row,
@@ -15,7 +24,8 @@ import {
     ControlLabel,
     Modal,
     FieldGroup
-} from 'react-bootstrap'
+}
+from 'react-bootstrap'
 import DragList from './DragList'
 
 import fileTagActions from '../action/fileTagAction'
@@ -26,160 +36,212 @@ import '../style/file-uploader.scss'
 
 class FileTopIndicator extends React.Component {
 
-    constructor() {
-        super()
-        this.state = {
-            showModal: false,
-            value: ''
+        constructor() {
+            super()
+            var fd = new FormData();
+            fd.append('testname1', 'testvalue1');
+            fd.append('testname1', 'testvalue2');
+            fd.append('file',null)
+            this.state = {
+                showModal: false,
+                uploadFormData: fd
+            }
+            this.handleUploadFileChange = this.handleUploadFileChange.bind(this);
+            this.handleUploadFormSubmit = this.handleUploadFormSubmit.bind(this);
+            this.handleChangeTag = this.handleChangeTag.bind(this);
+            // this.handleChange2= this.handleChange2.bind(this);
         }
-        this.submit = this.submit.bind(this);
-        this.handleChangeTag = this.handleChangeTag.bind(this);
-        // this.handleChange2= this.handleChange2.bind(this);
-    }
 
-    close() {
-        this.setState({showModal: false});
-    }
+        close() {
+            this.setState({
+                showModal: false
+            });
+        }
 
-    open() {
-        this.setState({showModal: true});
-    }
+        open() {
+            this.setState({
+                showModal: true
+            });
+        }
 
-handleChangeTag(event){
-    // console.log(event.target.value)
-this.props.actions.setInputFileTag(event.target.value);
+        handleChangeTag(event) {
+            // console.log(event.target.value)
+            this.props.actions.setInputFileTag(event.target.value);
+        }
+
+        // handleChange2(event){
+        // this.myInput.value = event.target.value;
+        // }
+
+        handleUploadFileChange(event) {
+            let fileNum = event.target.files.length;
+            console.log('选择的文件数目', fileNum);
+            let formData = this.state.uploadFormData;
+
+            const filesToUpload = event.target.files;
+            // console.log(filesToUpload.length, filesToUpload)
+            for (let i = 0, len = filesToUpload.length; i < len; i++) {
+                formData.append('file', filesToUpload[i])
+            }
+            console.log(formData.get('file'));
+
+            this.setState({
+                uploadFormData: formData
+            });
+        }
+
+        handleUploadFormSubmit(event) {
+            event.preventDefault()
+
+            let formData = this.state.uploadFormData;
+            formData.append('file_tag', this.props.fTag);
+            formData.append('user_id', 'supersu');
+            // formData.append('input2',this.myInput.value)
+
+            // const filesToUpload = this.fileInput.files;
+            // console.log(filesToUpload.length, filesToUpload)
+            // for (let i = 0, len = filesToUpload.length; i < len; i++) {
+            //     formData.append('file', filesToUpload[i])
+            // }
+            this.setState({
+                uploadFormData: formData
+            });
+
+            console.log(this.state.uploadFormData.get('file'))
+                // console.log(formData)
+
+            fetch('/file/upload', {
+                method: 'POST',
+                body: this.state.uploadFormData
+            }).then(function(response) {
+                console.log("上传formData成功")
+                console.log('响应结果', response);
+
+            }).catch(function(err) {
+                console.log("上传失败")
+            });
+
+        }
+
+
+        render() {
+
+            let {
+                fTag, actions
+            } = this.props;
+
+            return ( < div >
+                    < Grid >
+                    < Row >
+                    < Col md = {
+                        3
+                    } > < a href = "/#/filemap" > < i className = "fa fa-map-marker fa-2x" > < /i></a > < Button bsClass =
+                    "btn btn-all-files opacity75 to-m-left8" > 所有文件 < /Button></Col >
+                    < Col md = {
+                        4
+                    } > < /Col> < Col md = {
+                    2
+                } >
+                < Button id = "btnUpload"
+            bsClass = "btn btn-upload to-m-left8"
+            onClick = {
+                this.open.bind(this)
+            } > 上传 < /Button> < Button id = "btnNew"
+            bsClass = "btn btn-default to-m-left20" > 新建 < /Button> < /Col > < Col md = {
+                    2
+                } > < FormGroup >
+                < InputGroup >
+                < InputGroup.Button >
+                < Button > 搜索 < /Button> < /InputGroup.Button > < FormControl type = "text"
+            placeholder = "输入文件名" / >
+                < /InputGroup> < /FormGroup > < /Col> < Col md = {
+            1
+        } > < /Col> < /Row > < /Grid>
+
+
+        < Modal show = {
+            this.state.showModal
+        }
+        onHide = {
+                this.close.bind(this)
+            } >
+            < Modal.Header closeButton >
+            < Modal.Title > < span className = "font-file-name" > 上传文件到云盘 < /span></Modal.Title >
+            < /Modal.Header> < form onSubmit = {
+        this.handleUploadFormSubmit
+    } >
+    < Modal.Body >
+    < div > < span className = "font-file-list" > 上传到： & frasl; < /span></div >
+
+< div >
+    < div className = "display-inline-block" >
+    < label htmlFor = "file"
+className = "btn btn-default btn-upload" >
+    < i className = "fa fa-plus fa-1x" > < /i> &nbsp;&nbsp;添加文件 < input type = "file"
+className = "file-input opacity0"
+ref = {
+    (c) => {
+        this.fileInput = c
+    }
 }
+name = "file"
+onChange = {
+    this.handleUploadFileChange
+}
+multiple / >
+    < /label> < /div >
 
-// handleChange2(event){
-// this.myInput.value = event.target.value;
-// }
 
-    submit(e) {
-        e.preventDefault()
+    < div className = "display-inline-block to-m-left8" >
+    < div > < span className = "font-file-list" > 添加标签： < /span></div >
+    < input type = "text"
+value = {
+    fTag
+}
+onChange = {
+    this.handleChangeTag
+}
+/>
 
-        let formData = new FormData();
-        formData.append('file_tag',this.props.fTag)
-        formData.append('user_id','supersu')
-        // formData.append('input2',this.myInput.value)
 
-        const filesToUpload = this.fileInput.files;
-        console.log(filesToUpload.length,filesToUpload)
-        for(let i=0,len=filesToUpload.length;i<len;i++){
-            formData.append('file', filesToUpload[i])
+< /div>
 
+< /div>
+
+< div > { /*<FileDropbox ></FileDropbox>*/ }
+
+< div id = "dropbox" >
+    < div className = "font-file-list" >
+    < DragList inputFiles = {
+        this.state.uploadFormData.get('file')
+    } >
+    < /DragList> < /div > < /div> < /div >
+
+    < div >
+    < fieldset id = "progress"
+style = {
+        {
+            display: 'inline'
         }
+    } >
+    < div className = "progress-trough" >
+    < div id = "progress-bar"
+className = "progress-bar" >
+    < span className = "progress-bar-text" > 0 % < /span> < /div > < /div> < /fieldset > < /div>
 
-        console.log(formData.get('file'))
-        // console.log(formData)
+< /Modal.Body> < Modal.Footer id = "uploadFooter" > < Button onClick = {
+this.close.bind(this)
+} > 取消 < /Button> < button type = 'submit'
+className = "btn btn-upload " > 上传 < /button>
 
-        fetch('/file/upload', {
-            method: 'POST',
-            body: formData
-        }).then(function (response) {
-            console.log("上传formData成功")
-            console.log('响应结果',response);
+< /Modal.Footer> < /form >
 
-        }).catch(function (err) {
-            console.log("上传失败")
-        });
+< /Modal>
 
-    }
+< /div>
 
-
-    render() {
-
-let{fTag,actions}=this.props;
-
-        return (
-            <div>
-                <Grid>
-                    <Row >
-                        <Col md={3}> <a href="/#/filemap"><i className="fa fa-map-marker fa-2x"></i></a> <Button bsClass="btn btn-all-files opacity75 to-m-left8" >所有文件</Button></Col>
-                        <Col md={4}> </Col>
-                        <Col md={2}>
-                            <Button id="btnUpload" bsClass="btn btn-upload to-m-left8"
-                                    onClick={this.open.bind(this)}>上传</Button>
-                            <Button id="btnNew" bsClass="btn btn-default to-m-left20">新建</Button>
-                        </Col>
-                        <Col md={2}> <FormGroup>
-                            <InputGroup>
-                                <InputGroup.Button>
-                                    <Button>搜索</Button>
-                                </InputGroup.Button>
-                                <FormControl type="text" placeholder="输入文件名"/>
-                            </InputGroup>
-                        </FormGroup>
-                        </Col>
-                        <Col md={1}></Col>
-                    </Row>
-                </Grid>
-
-
-                <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
-                    <Modal.Header closeButton>
-                        <Modal.Title> <span className="font-file-name">上传文件到云盘</span></Modal.Title>
-                    </Modal.Header>
-                    <form onSubmit={this.submit}  >
-                        <Modal.Body>
-                            <div><span className="font-file-list">上传到： &frasl;</span></div>
-
-                            <div>
-                                <div className="display-inline-block">
-                                    <label htmlFor="file[]" className="btn btn-default btn-upload">
-                                        <i className="fa fa-plus fa-1x"></i> &nbsp;&nbsp;添加文件
-                                    <input type="file"  className="file-input opacity0" ref={(c)=>{this.fileInput=c}} name="file[]"   multiple/>
-                                    </label>
-                                </div>
-
-
-                                <div className="display-inline-block to-m-left8">
-                                    <div><span className="font-file-list">添加标签：</span></div>
-                                        <input type="text"  value={fTag} onChange={this.handleChangeTag}/>
-                                    {/*<input type="text"  defaultValue="第二项input" ref=(i)=>this.myInput=i onChange=this.handleChange2/>*/}
-
-                                </div>
-
-                            </div>
-
-                            <div>
-                                {/*<FileDropbox ></FileDropbox>*/}
-
-                                <div id="dropbox">
-                                    <div className="font-file-list">
-                                    <DragList>
-                                        <span>可以把文件拖到这里</span>
-                                            <span>无文件</span>
-                                        <span>上船吧</span>
-                                    </DragList>
-                                </div>
-                                </div>
-                            </div>
-
-                            <div>
-                                <fieldset id="progress" style={{display: 'inline'}}>
-                                    <div className="progress-trough">
-                                        <div id="progress-bar" className="progress-bar">
-                                            <span className="progress-bar-text">0%</span>
-                                        </div>
-                                    </div>
-                                </fieldset>
-                            </div>
-
-                        </Modal.Body>
-                        <Modal.Footer id="uploadFooter">
-                            <Button onClick={this.close.bind(this)}>取消</Button>
-                            <button type='submit' className="btn btn-upload ">上传
-                            </button>
-
-                        </Modal.Footer>
-                    </form>
-
-                </Modal>
-
-            </div>
-
-        )
-    }
+)
+}
 
 }
 
